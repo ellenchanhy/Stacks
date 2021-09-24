55f46a33e561d0dafaa35229671e118176db6ae4
b-kelly <b-kelly@users.noreply.github.com>
2021-09-24 13:48:16 -0400

    update package.json and references

--- docs/assets/js/controllers/stimulus.d.ts
+++ docs/assets/js/controllers/stimulus.d.ts
@@ -1,4 +1,4 @@
 // allows stimulus to be used as a UMD global from TypeScript.
 // You can delete this file after https://github.com/stimulusjs/stimulus/issues/238 has been fixed
-export * from "stimulus";
+export * from "@hotwired/stimulus";
 export as namespace Stimulus;
